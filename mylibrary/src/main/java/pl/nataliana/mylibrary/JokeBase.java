8fb3384b82d4dbd5e9fed035b07f58fa81cce653
Natalia Nazaruk <nazarjazz@wp.pl>
2018-06-01 14:30:51 +0200

    Android library added

--- mylibrary/src/main/java/pl/nataliana/mylibrary/JokeBase.java
+++ mylibrary/src/main/java/pl/nataliana/mylibrary/JokeBase.java
@@ -1,5 +1,11 @@
 package pl.nataliana.mylibrary;
 
+import android.view.View;
+import android.widget.Toast;
+
 public class JokeBase {
 
+    public void tellJoke(View view) {
+        Toast.makeText(this, MainActivity.joke1, Toast.LENGTH_SHORT).show();
+    }
 }
