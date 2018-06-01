8c387b067de7cd4e3dfd1dc7f4e610330b6f5933
Natalia Nazaruk <nazarjazz@wp.pl>
2018-06-01 16:35:25 +0200

    Fragment deleted

--- androidLib/src/main/java/pl/nataliana/mylibrary/JokeBase.java
+++ androidLib/src/main/java/pl/nataliana/mylibrary/JokeBase.java
@@ -2,12 +2,13 @@ package pl.nataliana.mylibrary;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 //This is my android library
 public class JokeBase extends AppCompatActivity {
 
     public static String JOKE_KEY = "joke-key";
-
+    private TextView myJokeTV;
     private String jokeFromLib = "";
 
     @Override
@@ -18,11 +19,7 @@ public class JokeBase extends AppCompatActivity {
         if (getIntent().getStringExtra(JokeBase.JOKE_KEY) != null) {
             jokeFromLib = getIntent().getStringExtra(JokeBase.JOKE_KEY);
         }
-
-        getSupportFragmentManager()
-                .beginTransaction().add(
-                R.id.joke_fragment,
-                JokeFragment.newInstance(jokeFromLib))
-                .commit();
+        myJokeTV = findViewById(R.id.tv_myjoke);
+        myJokeTV.setText(jokeFromLib);
     }
 }
