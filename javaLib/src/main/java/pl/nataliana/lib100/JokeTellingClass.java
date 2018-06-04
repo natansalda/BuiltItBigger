65f8ca9b1aa2976e4e607028c16ea815f717136f
Natalia Nazaruk <nazarjazz@wp.pl>
2018-06-04 21:15:14 +0200

    few jokes provided

--- javaLib/src/main/java/pl/nataliana/lib100/JokeTellingClass.java
+++ javaLib/src/main/java/pl/nataliana/lib100/JokeTellingClass.java
@@ -3,8 +3,8 @@ package pl.nataliana.lib100;
 public class JokeTellingClass {
 
     private String[] jokes = {"Can a kangaroo jump higher than a house? \n" +
-            "\n - Of course, a house doesn’t jump at all.",
-            "Anton, do you think I’m a bad mother?\n" +
+            "\n - Of course, a house does not jump at all.",
+            "Anton, do you think I am a bad mother?\n" +
                     "\n" +
                     "My name is Paul.",
             "My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away."};
